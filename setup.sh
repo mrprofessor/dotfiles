56b591659d25940b77713fb67cbcf00f0ac6aeba
mrprofessor <contact@rudra.dev>
2022-09-16 01:55:53 +0530

    Remove tools that aren't my daily anymore

--- setup.sh
+++ setup.sh
@@ -22,9 +22,9 @@ source ~/.zshrc
 ############################################################
 # Alacritty setup - A faster terminal emulator
 ############################################################
-brew cask install alacritty
-mkdir -p ~/.config/alacritty/ # Create alacritty config folder
-ln -s ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+#brew cask install alacritty
+#mkdir -p ~/.config/alacritty/ # Create alacritty config folder
+#ln -s ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 
 ############################################################
@@ -52,8 +52,8 @@ nvim +PluginInstall +qall
 ############################################################
 # Emacs - Old man yells at editor
 ############################################################
-brew tap d12frosted/emacs-plus
-brew install emacs-plus@28
+#brew tap d12frosted/emacs-plus
+#brew install emacs-plus@28
 
 
 ############################################################
@@ -74,10 +74,10 @@ brew install ripgrep
 ############################################################
 # DOOM - Vim inside emacs
 ############################################################
-git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-~/.emacs.d/bin/doom install
+#git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+#~/.emacs.d/bin/doom install
 
-mkdir -p ~/.doom.d/
-ln -f ~/dotfiles/doom.d/config.el ~/.doom.d/config.el
-ln -f ~/dotfiles/doom.d/init.el ~/.doom.d/init.el
-ln -f ~/dotfiles/doom.d/packages.el ~/.doom.d/packages.el
+#mkdir -p ~/.doom.d/
+#ln -f ~/dotfiles/doom.d/config.el ~/.doom.d/config.el
+#ln -f ~/dotfiles/doom.d/init.el ~/.doom.d/init.el
+#ln -f ~/dotfiles/doom.d/packages.el ~/.doom.d/packages.el
