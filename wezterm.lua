adc9d7fe2a6c3002e224279cc917987688b0ca33
mrprofessor <contact@rudra.dev>
2024-08-28 13:00:44 +1200

    Simplify tmux

--- wezterm.lua
+++ wezterm.lua
@@ -60,7 +60,7 @@ end)
 -- }
 
 -- Options if I feel like it in the future
--- config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = true
 -- config.tab_bar_at_bottom = true
 
 
